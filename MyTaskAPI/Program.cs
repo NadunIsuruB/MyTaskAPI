f74c6abeb3dd38d503ce80a11cc56e258448905e
Nadun Nimalarathna <nadun.n@azendtech.com>
2023-03-16 13:22:50 +0530

    Scaffold a controller

--- MyTaskAPI/Program.cs
+++ MyTaskAPI/Program.cs
@@ -1,9 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using MyTaskAPI.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<TodoContext>(opt =>
+    opt.UseInMemoryDatabase("Todos"));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
